072c57250f89cfa020bb8882e42516df2df67fac
хpιツ <123358878+xpi-ay@users.noreply.github.com>
2023-03-20 10:05:42 +0000

    Add files via upload

---
+++ Turtle2.py
@@ -0,0 +1,26 @@
+import turtle
+triangolo = 3
+quadrato = 4
+pentagono = 5
+esagono = 6
+
+i = 0
+t = turtle.Pen()
+lati = int(input("inserisci il numero dei lati"))
+forma = ''
+forma = input('inserisci il nome della forma geometrica')
+if lati<=2:
+    print("non è possibile disegnare nessuna forma geometrica ")
+elif 
+elif lati >= 3:
+    while i < lati:
+        t.right(120)
+        t.forward(100)
+        i+=1
+
+
+
+
+
+
+
